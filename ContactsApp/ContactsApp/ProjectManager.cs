48d0b2aa021dc1c9cb6dc8fbaf81cc0774dbeb5d
Vladuxexel <55832677+Vladuxexel@users.noreply.github.com>
2020-09-29 23:21:58 +0700

    Добавлена библиотека Newtonsoft JSON.NET. Добавлен класс сериализации.

---
+++ ContactsApp/ContactsApp/ProjectManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ContactsApp
+{
+    /// <summary>
+    /// Класс менеджера проекта.
+    /// </summary>
+    public static class ProjectManager
+    {
+        /// <summary>
+        /// Путь для сериализации/десериализации данных проекта.
+        /// </summary>
+        private const string FileName = "ContactsApp.notes";
+
+        /// <summary>
+        /// Метод сериализации данных проекта.
+        /// </summary>
+        public static void SerializeProject(Project project, string path)
+        {
+            path += FileName;
+
+            var serializer = new JsonSerializer();
+
+            using (var sw = new StreamWriter(path))
+            using (var writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, project);
+            }
+        }
+
+        /// <summary>
+        /// Метод десериализации данных проекта.
+        /// </summary>
+        public static Project DeserializeProject(string path)
+        {
+            path += FileName;
+
+            Project project = null;
+
+            var serializer = new JsonSerializer();
+
+            using (var sr = new StreamReader(path))
+            using (var reader = new JsonTextReader(sr))
+            {
+                project = (Project)serializer.Deserialize<Project>(reader);
+            }
+
+            return project;
+        }
+    }
+}
