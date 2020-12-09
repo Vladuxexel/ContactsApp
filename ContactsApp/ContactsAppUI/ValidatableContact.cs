dca8ead2ed5ebd31b934967301b0be6eb902bebc
Vladuxexel <55832677+Vladuxexel@users.noreply.github.com>
2020-12-09 09:39:06 +0700

    Убраны лишние ссылки

--- ContactsApp/ContactsAppUI/ValidatableContact.cs
+++ ContactsApp/ContactsAppUI/ValidatableContact.cs
@@ -1,10 +1,6 @@
 ﻿using ContactsApp;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ContactsAppUI
 {
